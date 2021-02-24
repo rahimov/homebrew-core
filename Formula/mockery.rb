8d7bdacef051b3e12666419b9aaffa2db0ae641a
Рахимов Александр <rahimov@tochka.com>
2021-02-24 23:12:56 +0500

    downgrade mockery version to 2.3.0

--- Formula/mockery.rb
+++ Formula/mockery.rb
@@ -1,18 +1,12 @@
 class Mockery < Formula
   desc "Mock code autogenerator for Golang"
   homepage "https://github.com/vektra/mockery"
-  url "https://github.com/vektra/mockery/archive/v2.6.0.tar.gz"
-  sha256 "ce5eea62e9d130e3557c11016c306485ccd77919cf85d706a2556bce28d085ed"
+  url "https://github.com/vektra/mockery/archive/v2.3.0.tar.gz"
+  sha256 "ec373ef32fccdd1b9277e0de7c19b37fb1b64d7a230831cc0d41033f554e15df"
   license "BSD-3-Clause"
   revision 1
   head "https://github.com/vektra/mockery.git"
 
-  bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "838bc4e94d6889dab090984c0f70062d5a6e65c25076eb891140e0caa80cb3b7"
-    sha256 cellar: :any_skip_relocation, big_sur:       "775e3c5ab4ff9baa1e464a3402cc5df4d40938f8e1830743e713e48dbefc6861"
-    sha256 cellar: :any_skip_relocation, catalina:      "8b4d00f30efe40382d919e3deebbb89cedb178c9b7a26be8008105bd8b8ecb9d"
-    sha256 cellar: :any_skip_relocation, mojave:        "95b2ea44dbbc182f2b870a3526fc41abd98b43dcb700d6a4b10f789229a17ed2"
-  end
 
   depends_on "go"
 
